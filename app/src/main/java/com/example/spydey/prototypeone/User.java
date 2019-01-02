6cfd1370225fe1fbfe012e278e38a4397d7117a2
Prathamesh Pavnoji <prathameshh27@gmail.com>
2019-01-03 01:25:09 +0530

    applied layout changes to Register and signin activity. Added:      Gender field to the Register activity.      Actions to the Home Activity.      New activity Called Analyse without logic.

--- app/src/main/java/com/example/spydey/prototypeone/User.java
+++ app/src/main/java/com/example/spydey/prototypeone/User.java
@@ -5,15 +5,16 @@ package com.example.spydey.prototypeone;
  */
 
 public class User {
-    public String firstName, lastName, email, phone, birth;
+    public String firstName, lastName, email, phone, birth, gender;
 
     public User() {}
 
-    public User(String fName, String lName, String email, String phone, String birth) {
+    public User(String fName, String lName, String email, String phone, String birth, String gender) {
         this.firstName = fName;
         this.lastName = lName;
         this.email = email;
         this.phone = phone;
         this.birth = birth;
+        this.gender = gender;
     }
 }
